81804f19b8bb25ae163b32fba170a50e1333d22c
wcalvert88 <wcalvert88@gmail.com>
2018-06-16 16:07:01 -0400

    learned about global values

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -3,11 +3,22 @@ namespace App\Twig;
 
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
+use Twig\Extension\GlobalsInterface;
 
-class AppExtension extends AbstractExtension{
+class AppExtension extends AbstractExtension implements GlobalsInterface {
+    public function __construct(string $locale) {
+        $this->locale = $locale;
+    }
+
     public function getFilters() {
         return [
             new TwigFilter('price', [$this, 'priceFilter'])
+        ];
+    }
+
+    public function getGlobals() {
+        return [
+            'locale' => $this->locale
         ];
     }
 
