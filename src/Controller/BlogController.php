61099ea725918a2fba302d2c5b406b25cd7725b6
wcalvert88 <wcalvert88@gmail.com>
2018-06-16 09:58:49 -0400

    learned parameter binding, manual service wiring, and service tags

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -3,27 +3,32 @@
 namespace App\Controller;
 
 use App\Service\Greeting;
+use App\Service\VeryBadDesign;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BlogController extends AbstractController
  {
-     /**
-      * @var Greeting
-      */
-      private $greeting;
+    /**
+     * @var Greeting
+    */
+    private $greeting;
+    /**
+     * @var VeryBadDesign
+     */
+    private $badDesign;
+    public function __construct(Greeting $greeting, VeryBadDesign $badDesign) {
+        $this->greeting = $greeting;
+        $this->badDesign = $badDesign;
+    }
 
-      public function __construct(Greeting $greeting) {
-          $this->greeting = $greeting;
-      }
-
-      /**
-       * @Route("/", name="blog_index")
-       */
-      public function index(Request $request) {
-          return $this->render('base.html.twig', ['message' => $this->greeting->greet($request->get('name'))]);
-      }
+    /**
+     * @Route("/", name="blog_index")
+     */
+    public function index(Request $request) {
+        return $this->render('base.html.twig', ['message' => $this->greeting->greet($request->get('name'))]);
+    }
 
 
 }
